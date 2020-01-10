32dcd7775db0028716b7e983f353f1d046f3c3c4
juris124 <31391698+juris124@users.noreply.github.com>
2020-01-10 17:13:04 +0200

    Chats v0.4

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 from flask import Flask, render_template, jsonify, request
-
-
 app = Flask('app')
-
-
 @app.route('/')
 def index_page():
-  return render_template("chats.html")
+    return render_template("chats.html")
+
 
 @app.route('/helth')
 def helth():
-  return "OK"
+    return "OK"
+
 
 @app.route('/chats/lasi')
 def ielasit_chatu():
-  chata_rindas=[]
-  with open("chats.txt", "r", encoding="utf-8") as f:
-    for rinda in f:
-      chata_rindas.append(rinda)
+    chata_rindas = []
+    with open("chats.txt", "r", encoding="UTF-8") as f:
+        for rinda in f:
+            chata_rindas.append(rinda)
+    return jsonify({"chats": chata_rindas})
 
-  return jsonify({"chats":chata_rindas})
 
 @app.route('/chats/suuti', methods=['POST'])
 def suti_zinju():
-  dati = request.json
-  with open("chats.txt", "a", newline="") as f:
-    f.write(dati["chats"] + "\n")
-
-  return ielasit_chatu() 
+    dati = request.json
+    with open("chats.txt", "a", newline="", encoding="UTF-8") as f:
+        f.write(dati["chats"] + "\n")
+    return ielasit_chatu()
 
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
+        # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000, debug=True)
--- static/chats.js
+++ static/chats.js
@@ -4,7 +4,7 @@ const ATJAUNOT = 3000;
 async function lasiChatu(){
     const atbilde = await fetch('/chats/lasi');
     const datuObjekts = await atbilde.json();
-    raadiChatuVienkaarshi(datuObjekts);
+    raadiChataRindas(datuObjekts);
 
     await new Promise(resolve => setTimeout(resolve, ATJAUNOT));
     await lasiChatu();
@@ -38,7 +38,7 @@ async function suutiZinju(){
 
     const datuObjekts = await atbilde.json();
 
-    raadiChatuVienkaarshi(datuObjekts);
+    raadiChataRindas(datuObjekts);
 }
 
 let ievadesLauks = document.getElementById("zinja");
@@ -47,3 +47,30 @@ ievadesLauks.addEventListener("keyup", function(event){
         suutiZinju();
     }
 })
+
+function raadiChataRindas(dati) {
+    const chatUL = document.getElementById("chats");
+    // novaacam ieprieksheejo saturu
+    while (chatUL.firstChild) {
+        chatUL.firstChild.remove();
+    }
+    for (let rinda of dati["chats"]) {
+      chatLI = izveidoJaunuRindu(rinda);
+      chatUL.appendChild(chatLI);
+    }
+    // noskrolleejam uz leju pie peedeejaa chata texta
+    var chatScrollBox = chatUL.parentNode;
+    chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
+  }
+  
+  
+  function izveidoJaunuRindu(zinja) { 
+    let newLI = document.createElement("li");
+    newLI.className = "left clearfix"
+    let newDiv = document.createElement("div"); 
+    newDiv.className = "chat-body clearfix"
+    let newContent = document.createTextNode(zinja); 
+    newLI.appendChild(newDiv); 
+    newDiv.appendChild(newContent); 
+    return newLI;
+  }
