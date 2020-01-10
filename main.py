536833042679e91cd4255ff534585eeba9bcce74
juris124 <31391698+juris124@users.noreply.github.com>
2020-01-10 12:19:59 +0200

    cats v0.1 1.uzd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask('app')
@@ -6,9 +6,13 @@ app = Flask('app')
 
 @app.route('/')
 def index_page():
-  return "Sveiciens no Jura 10.janvārī!"
+  return render_template("index.html")
+
+@app.route('/helth')
+def helth():
+  return "OK"
 
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
