58d0c70995680542014868df2a50f843fc2e0eab
juris124 <31391698+juris124@users.noreply.github.com>
2020-01-10 15:22:31 +0200

    Chata lasiishana v0.3.1

--- static/chats.js
+++ static/chats.js
@@ -1,1 +1,29 @@
-console.log("Sveiciens no konsoles!");
+//console.log("Sveiciens no konsoles!");
+const ATJAUNOT = 5000;
+
+async function lasiChatu(){
+    const atbilde = await fetch('/chats/lasi');
+    const datuObjekts = await atbilde.json();
+    raadiChatuVienkaarshi(datuObjekts);
+}
+
+function raadiChatuVienkaarshi(dati){
+    const jaunaRinda="<br>";
+    let chats = "";
+    let chataDiv = document.getElementById("chats");
+
+    for(let rinda of dati['chats']){
+        chats = chats + rinda + jaunaRinda;
+
+    }
+    chataDiv.innerHTML = chats;
+}
+
+function raadiChatu(){
+
+}
+
+
+function suutiZinju(){
+
+}
